d8e49d257b786b38c8762b211520b9b10b33350d
LEVISSE Etienne <elevisse@itsgroup.fr>
2019-07-04 16:18:59 +0200

    OK pour le lab 2

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,7 @@ Vagrant.configure("2") do |config|
 	node1.vm.box = "debian/buster64"
 	node1.vm.hostname = "node1"
 	node1.vm.network "private_network", ip: "10.0.3.154", virtualbox__intnet: "lab_ansible"
+  node1.vm.network "forwarded_port", guest: 80, host: 8080, auto_correct: true
 	node1.vm.provider "virtualbox" do |node1_vb|
       node1_vb.cpus = 2
       node1_vb.memory = 2048
--- scripts_va/ansible.sh
+++ scripts_va/ansible.sh
@@ -9,7 +9,6 @@ vagrant
 vagrant
 EOF
 echo "10.0.3.154 node1" >> /etc/hosts
-echo "10.0.3.155 node2" >> /etc/hosts
 mkdir -p /etc/ansible
 echo "# config file for ansible -- https://ansible.com/" > /etc/ansible/ansible.cfg
 echo "# ===============================================" >> /etc/ansible/ansible.cfg
@@ -17,7 +16,7 @@ echo '[defaults]' >> /etc/ansible/ansible.cfg
 echo "inventory      = /etc/ansible/hosts" >> /etc/ansible/ansible.cfg
 echo 'host_key_checking = False' >> /etc/ansible/ansible.cfg
 echo "node1" > /etc/ansible/hosts
-echo "node2" >> /etc/ansible/hosts
 su - ansible -c "ssh-keygen -f /home/ansible/.ssh/id_rsa -q -P ''"
+su - ansible -c "ssh-keyscan -H node1 >> /home/ansible/.ssh/known_hosts"
 echo "vagrant" > /home/ansible/password.txt
-su - ansible -c "sshpass -f password.txt ssh-copy-id node1; true"
+su - ansible -c "sshpass -f password.txt ssh-copy-id node1 -f; true"
